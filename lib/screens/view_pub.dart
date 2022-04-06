ccb898147ee9b848c3425d98171dcddd139a5b1c
Jhon Ruiz <jhonjairo.45@hotmail.com>
2022-04-06 00:32:54 -0500

    Container de publicacion

--- lib/screens/view_pub.dart
+++ lib/screens/view_pub.dart
@@ -41,6 +41,67 @@ class _viewPubState extends State<viewPub> {
     final userName = "Jhon Ruiz";
     final fecha = "2/04/2022";
 
+    Container _contPub = Container(
+      //color: Colors.deepPurpleAccent,
+      padding: EdgeInsets.all(resposive.dp(2)),
+      width: resposive.wp(80),
+      height: resposive.hp(60),
+      decoration: BoxDecoration(
+        color: Colors.amber,
+        borderRadius: BorderRadius.all(
+          Radius.circular(
+            resposive.dp(7),
+          ),
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            children: [
+              CircleAvatar(
+                radius: resposive.dp(4.5),
+                backgroundImage: AssetImage('assets/images/a.jpg'),
+              ),
+              SizedBox(
+                width: resposive.wp(2),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        userName,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: resposive.dp(3),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        fecha,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: resposive.dp(1.5),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const Icon(Icons.public),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Publicaciones'),
@@ -79,56 +140,11 @@ class _viewPubState extends State<viewPub> {
                 ),
               ),
               Positioned(
-                //top: pinkSize * 0.03,
-                child: Container(
-                  padding: EdgeInsets.all(resposive.dp(1)),
-                  child: Column(
-                    children: <Widget>[
-                      Row(
-                        children: [
-                          CircleAvatar(
-                            radius: resposive.dp(4.5),
-                            backgroundImage: AssetImage('assets/images/a.jpg'),
-                          ),
-                          SizedBox(
-                            width: resposive.wp(2),
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(
-                                children: [
-                                  Text(
-                                    userName,
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      fontSize: resposive.dp(3),
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  Text(
-                                    fecha,
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      fontSize: resposive.dp(1.5),
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  const Icon(Icons.public),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                ),
+                top: pinkSize * 0.15,
+                left: resposive.wp(11),
+                child: _contPub,
               ),
+              //?
             ],
           ),
         ),
