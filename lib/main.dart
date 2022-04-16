34222d7c2cf47515528569a757e5ddfa247e86d0
JhonRuiz95 <jhonjairo.45@hotmail.com>
2022-04-16 18:58:21 -0500

    Quitando un import de la pagina principal

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_3/screens/login_screen.dart';
 import 'package:flutter_application_3/screens/raiz.dart';
 
 Future<void> main() async {
